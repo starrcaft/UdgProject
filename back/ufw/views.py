00a855147729b2771811c0e910a961ef53f6cca6
starrcaft <starrcaft@naver.com>
2019-11-21 14:17:48 +0900

    add templates

--- back/ufw/views.py
+++ back/ufw/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Log
 
 # Create your views here.
+
+def list(request):
+    logs = Log.objects.filter()
+    return render(request, 'ufw/a.html', {})
