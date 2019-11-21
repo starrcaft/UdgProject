1e616cfc2a9125aad8bc93e07f27c3c28cfe6258
deu04322 <deu04322@naver.com>
2019-11-21 22:03:21 +0900

    Update inputLog.py

--- back/back/logContorller/inputLog.py
+++ back/back/logContorller/inputLog.py
@@ -1,2 +1,21 @@
 
 #input
+
+import os
+import threading
+import time
+
+def thread_run():
+	result = os.popen("tail -n 10 /var/log/syslog | egrep '(approach log : |sudo)'").read()
+	result2 = os.popen('tail -n 5 /var/log/auth.log').read()
+	print(result)
+	print(result2)
+	
+	threading.Timer(10, thread_run).start()
+
+def main():
+	thread_run()
+
+
+if __name__=="__main__":
+	main()
