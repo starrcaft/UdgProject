821ddb301a62f474e0e9b35bf04524b2d20fd0ee
starrcaft <starrcaft@naver.com>
2019-11-21 22:12:30 +0900

    add inputlog.py

---
+++ back/logController/inputLog.py
@@ -0,0 +1,11 @@
+import os
+import threading
+import time
+
+def thread_run():
+	result = os.popen("tail -n 10 /var/log/syslog | egrep '(approach log : |sudo)'").read()
+	result2 = os.popen('tail -n 5 /var/log/auth.log').read()
+	print(result)
+	print(result2)
+
+	threading.Timer(10, thread_run).start()
