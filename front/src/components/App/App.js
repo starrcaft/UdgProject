523eaf4f7b32e82a54cb56844ebb64c851c543b5
starrcaft <starrcaft@naver.com>
2019-11-21 22:00:54 +0900

    reset

--- front/src/components/App/App.js
+++ front/src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Main from 'components/Main/Main';
+import Main from '../Main/Main';
 
 class App extends Component {
     state = {
--- front/src/components/SU/SU.js
+++ front/src/components/SU/SU.js
@@ -28,7 +28,7 @@ class SU extends Component {
                     <Card.Body>
                         <Card.Text>
                             Some quick example text to build on the card title and make up the bulk
-                            of the card's content.
+                            of the cards content.
                         </Card.Text>
                     </Card.Body>
                 </Card>
